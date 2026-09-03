854204cb0911e2295bc83985194caeac6ef9b053
Solas <ticofic4@gmail.com>
2026-09-03 06:41:09 +0300

    Fix formatting in executeOnClient method

--- src/main/java/rbasamoyai/escalated/platform/EnvExecute.java
+++ src/main/java/rbasamoyai/escalated/platform/EnvExecute.java
@@ -7,8 +7,8 @@ import java.util.function.Supplier;
 public class EnvExecute {
 
     public static void executeOnClient(Supplier<Runnable> run) {
-        if (FabricLoader.getInstance().getEnvironmentType().isClient())
+        if (FabricLoader.getInstance().getEnvironmentType().isClient()) {
             run.get().run();
+        }
     }
-
 }
